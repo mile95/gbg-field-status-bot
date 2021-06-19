33ba8145acd52ade4da8538969367b19f393f249
Fredrik Mile <fredrik.mile@gmail.com>
2021-06-19 21:45:07 +0200

    Added configuration.tf

---
+++ configuration.tf
@@ -0,0 +1,68 @@
+variable "AWS_ACCESS_KEY" {
+  description = "AWS_ACCESS_KEY"
+  type        = string
+  sensitive   = true
+}
+
+variable "AWS_SECRET_KEY" {
+  description = "AWS_SECRET_KEY"
+  type        = string
+  sensitive   = true
+}
+
+variable "TWITTER_ACCESS_TOKEN_SECRET" {
+  description = "TWITTER_ACCESS_TOKEN_SECRET"
+  type        = string
+  sensitive   = true
+}
+
+variable "TWITTER_ACCESS_TOKEN" {
+  description = "TWITTER_ACCESS_TOKEN"
+  type        = string
+  sensitive   = true
+}
+
+variable "TWITTER_CONSUMER_API_KEY" {
+  description = "TWITTER_CONSUMER_API_KEY"
+  type        = string
+  sensitive   = true
+}
+
+variable "TWITTER_CONSUMER_API_SECRET" {
+  description = "TWITTER_CONSUMER_API_SECRET"
+  type        = string
+  sensitive   = true
+}
+
+provider "aws" {
+    region = "eu-north-1"
+    access_key = var.AWS_ACCESS_KEY
+    secret_key = var.AWS_SECRET_KEY
+}
+
+resource "aws_lambda_function" "twitterbotTF" {
+    filename         = "lambda.zip"
+    function_name    = "twitterbotTF"
+    role             = "arn:aws:iam::753907798323:role/terra"
+    handler          = "lambda_function.lambda_handler"
+    source_code_hash = filebase64sha256("lambda.zip")
+    runtime          = "python3.8"
+    environment {
+        variables = {
+            ACCESS_TOKEN_SECRET = var.TWITTER_ACCESS_TOKEN_SECRET
+            ACCESS_TOKEN        = var.TWITTER_ACCESS_TOKEN
+            CONSUMER_API_KEY    = var.TWITTER_CONSUMER_API_KEY
+            CONSUMER_API_SECRET = var.TWITTER_CONSUMER_API_SECRET
+        }        
+    }
+}
+
+module "lambda-cloudwatch-trigger" {
+  source                        = "infrablocks/lambda-cloudwatch-events-trigger/aws"
+  region                        = "eu-north-1"
+  component                     = "twitterbotTF"
+  deployment_identifier         = "production"
+  lambda_arn                    = aws_lambda_function.twitterbotTF.arn
+  lambda_function_name          = "twitterbotTF"
+  lambda_schedule_expression    = "cron(0 8,10,13 ? * * *)"
+}
